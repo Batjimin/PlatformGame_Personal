973762e64758bf6d95b0b5149c38a082e7f47edf
Batjimin <147qqaa@hufs.ac.kr>
2023-06-11 21:11:28 +0900

    Delete useless color

--- src/Setting.py
+++ src/Setting.py
@@ -9,21 +9,13 @@ SCREEN_SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
 ## COLORS ##
 #                R    G    B
 GRAY         = (100, 100, 100)
-NAVYBLUE     = ( 60,  60, 100)
 WHITE        = (255, 255, 255)
 RED          = (255,   0,   0)
 GREEN        = (  0, 255,   0)
-FOREST_GREEN = ( 31, 162,  35)
-BLUE         = (  0,   0, 255)
-SKY_BLUE     = ( 39, 145, 251)
 YELLOW       = (255, 255,   0)
 ORANGE       = (255, 128,   0)
-PURPLE       = (255,   0, 255)
 BLACK        = (  0,   0,   0)
 NEAR_BLACK   = ( 19,  15,  48)
-GOLD         = (255, 215,   0)
-
-BGCOLOR = WHITE
 
 #배율
 SIZE_MULTIPLIER = 2.5
